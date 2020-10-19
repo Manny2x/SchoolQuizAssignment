bac47d6777d099bfa282dfd41b516d1e749c767c
Emmanuel Okafor <54027226+real-EOkafor@users.noreply.github.com>
2020-10-19 20:09:10 +0100

    Added Questions.java

---
+++ Questions.java
@@ -0,0 +1,25 @@
+interface Questions {
+  Question[] questions =
+    {
+    new Question("What do you do if someone asks for nudes?", "1. Okay but, you next", "2. No you pedophile eww ", 
+    "3. Okay but you go first ", 2), // Question 1
+    new Question("What do you do if your online friend asks to meet you?", "1. Sure my mum can take me", "2. Okay my mum said sure lets meet near my school", "3. No way do I know you that well no sorry", 3), // Question 2
+    new Question("What do you do if you see a mean picture of your friend?", "1. Report and block", "2. Laugh and spread it", "3. Tell your friend", 1), // Question 3
+    new Question("What do you do if you see a dating website profile that you never made?", "1. Talk to the people you matched with", "2. Report the account and remove the photos they used", "3. Make a new account and match with yourself", 2), // Question 4
+    new Question("What do you do if you getting threats from unknown people?", "1. Ask a police for help", "2. Become a detective and solve it yourself", "3. cry and cry", 1), // Question 5
+    new Question("What do you do if you someone replies with a rude comment on a post of yours?", "1. Ignore it/Block the person or report it if it is breaking TOS", "2. Argue back with the person", "3. Remove the post", 1), // Question 6
+    new Question("If something bad is happening in your life", "1. Post about it online", "2. Post about it online but on private", "3. Don't post it online", 2, 3), // Question 7
+    new Question("What setting should you keep your profile on", "1. Public", "2. Private", "3. Public to Followers", 2), // Question 8
+    new Question("What happens if you click on a shady website", "1. Tell an adult", "2.  Get scared", "3. Carry on with your day.", 1), // Question 9
+    new Question("You find your teachers social media online", "1. Report it", "2. Tell her/him about it", "3. Ignore it", 3), // Question 10
+    new Question("What do you do if you think your gettin catfished?", "1. Ask a team to help you not get catfished", "2. Block and report", "3. Continue the relationship and see how it goes", 1), // Question 11
+    new Question("You see a gory image on a SFW forum, what do you do?", "1. Repost it on the forum", "2. Ignore it", "3. Report it to the moderators", 3), // Question 12
+    new Question("What do you do if you get a ransom note in your inbox?", "1. Report it to the police", "2. Ignore it, it's probably fake", "3. Give them the money", 1), // Question 13
+    new Question("What do you do if someone beats you in a game?", "1. Complain and whine", "2. Man up, and play the game", "3. Break your controller", 2), // Question 14
+    new Question("What do you do if you get hacked", "1. Hack them back", "2. Report on website ", "3. Make a new account", 2, 3), // Question 15
+    new Question("What do you do if you forget your password?", "1. Request to reset it", "2. Make a post on a new account asking for help", "3. Create a new account and lose your progress", 2, 3), // Question 16
+    new Question("Whæt do you do if you see a fake account about you?", "1. Report", "2. Post saying its not me", "3. Ignore it", 1 , 2), // Question 17
+    new Question("What do you do if you get a email asking for an \" Irresistible investment! \"","1. Accept and make a fortune" , "2. Ignore it", "3. Block and report the email", 3), // Question 18
+    new Question("What do you do if you get banned?", "1. Accept your ban and don't do it again", "2. Create a new account and continue to break TOS", "3. Leave the website alone", 1, 3), // Question 19
+    new Question("What do you do if your information gets leaked?", "1. Leave and cry!", "2. Ignore it and let yourself be doxxed", "3. Report everything", 3),}; // Question 20
+}
